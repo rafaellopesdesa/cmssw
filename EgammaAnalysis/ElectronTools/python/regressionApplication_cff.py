c05298cabaaca5b07aa35ae120709304d15de9bd
Rafael <rclsa@fnal.gov>
2017-03-02 23:59:36 +0100

    producer for AOD

--- EgammaAnalysis/ElectronTools/python/regressionApplication_cff.py
+++ EgammaAnalysis/ElectronTools/python/regressionApplication_cff.py
@@ -1,20 +1,23 @@
 import FWCore.ParameterSet.Config as cms
 
-from PhysicsTools.PatAlgos.slimming.modifiedElectrons_cfi import modifiedElectrons
-from PhysicsTools.PatAlgos.slimming.modifiedPhotons_cfi import modifiedPhotons
 from EgammaAnalysis.ElectronTools.regressionModifier_cfi import regressionModifier
 
-slimmedElectrons = modifiedElectrons.clone()
-slimmedPhotons = modifiedPhotons.clone()
-
-regressionModifier.ecalrechitsEB = cms.InputTag("reducedEgamma:reducedEBRecHits")
-regressionModifier.ecalrechitsEE = cms.InputTag("reducedEgamma:reducedEERecHits")
-regressionModifier.useLocalFile  = cms.bool(False)
+regressionModifier.ecalrechitsEB = cms.InputTag("reducedEcalRecHitsEB")
+regressionModifier.ecalrechitsEE = cms.InputTag("reducedEcalRecHitsEE")
 
 egamma_modifications = cms.VPSet( )
 egamma_modifications.append( regressionModifier )
 
-slimmedElectrons.modifierConfig.modifications = egamma_modifications
-slimmedPhotons.modifierConfig.modifications   = egamma_modifications
+gedGsfElectrons = cms.EDProducer(
+    "ModifiedGsfElectronProducer",
+    src = cms.InputTag("gedGsfElectrons",processName=cms.InputTag.skipCurrentProcess()),
+    modifierConfig = cms.PSet( modifications = egamma_modifications )
+)
 
-regressionApplication = cms.Sequence( slimmedElectrons * slimmedPhotons )
+gedPhotons = cms.EDProducer(
+    "ModifiedGedPhotonProducer",
+    src = cms.InputTag("gedPhotons",processName=cms.InputTag.skipCurrentProcess()),
+    modifierConfig = cms.PSet( modifications = egamma_modifications )
+)
+
+regressionApplication = cms.Sequence( gedGsfElectrons * gedPhotons )
